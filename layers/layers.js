7fc302662143a639e14bc87aa1633a3c2730d831
Ploe4kaOfficial <60534938+Ploe4kaOfficial@users.noreply.github.com>
2025-04-05 20:49:57 +0900

    Update layers.js

--- layers/layers.js
+++ layers/layers.js
@@ -7,7 +7,8 @@ var wms_layers = [];
             
             
             source: new ol.source.XYZ({
-
+            attributions: ' &nbsp &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
             })
         });
 var format_3_1 = new ol.format.GeoJSON();
